12d10e925f15549f02a74a5072c63688e1e88586
Dmitry Litvinenko <ds-sev@ya.ru>
2023-05-13 10:59:39 +0300

    fix: edit url in api util

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -87,7 +87,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-59',
+  baseUrl: 'https://api.mesto.litvinenko-d.nomoredomains.monster',
   headers: {
     // authorization: 'c4ab66aa-531d-4641-bb6a-e0dfe4dabae8',
     'Content-Type': 'application/json',
