e5b53419df2ab280592340f991316ac035f8f93b
Dmitry Litvinenko <ds-sev@ya.ru>
2023-05-13 21:32:56 +0300

    feat: response data edit

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -61,7 +61,7 @@ function Main({
         <section className="cards wrapper" aria-label="Cards">
           {
             Array.isArray(cards)
-              ? cards.reverse((card) => (
+              ? cards.reverse.map((card) => (
                 <Card
                   title={card.name}
                   likes={card.likes}
