ee166b286f0ae70ef98797fa821a512bfb0a1821
Dmitry Litvinenko <ds-sev@ya.ru>
2023-05-15 11:08:14 +0300

    fix: use cors from npm

--- backend/app.js
+++ backend/app.js
@@ -4,32 +4,30 @@ const mongoose = require('mongoose')
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const { errors } = require('celebrate')
-// const cors = require('cors')
+const cors = require('cors')
 const routes = require('./routes/index')
-const cors = require('./middlewares/cors')
+// const cors = require('./middlewares/cors')
 const centralErrorHandler = require('./middlewares/centralErrorHandler')
 const { requestLogger, errorLogger } = require('./middlewares/logger')
 
 const app = express();
 const port = process.env.PORT || 3000
 
-// app.use(cors({
-//   origin: [
-//     'https://mesto.litvinenko-d.nomoredomains.monster',
-//     'https://mesto.litvinenko-d.nomoredomains.monster/users/me',
-//     'https://mesto.litvinenko-d.nomoredomains.monster/cards',
-//     'http://mesto.litvinenko-d.nomoredomains.monster',
-//     'https://mesto.litvinenko-d.nomoredomains.monster/#',
-//     'http://localhost',
-//     'http://localhost:3000',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   credentials: true,
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }))
-app.use(cors)
+app.use(cors({
+  origin: [
+    'https://mesto.litvinenko-d.nomoredomains.monster',
+    'https://mesto.litvinenko-d.nomoredomains.monster/users/me',
+    'https://mesto.litvinenko-d.nomoredomains.monster/cards',
+    'http://mesto.litvinenko-d.nomoredomains.monster',
+    'http://localhost',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 mongoose.connect(process.env.DB_CONN, {
   useNewUrlParser: true,
